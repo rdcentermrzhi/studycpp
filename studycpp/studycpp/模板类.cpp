d921822f27063f55187f8fa4002a4fe9b2b4fc27
whoami <2675204902@qq.com>
2018-09-03 17:00:07 +0800

    模板类继承相关

--- studycpp/studycpp/模板类.cpp
+++ studycpp/studycpp/模板类.cpp
@@ -13,15 +13,34 @@ public:
 		return t_;
 	}
 protected:
-private:
 	T t_;
+private:
+	
 };
+
+class ChildTpl :public TestTpl <int>
+{
+public:
+	ChildTpl(int i) :TestTpl<int>(i) 
+	{
+			
+	}
+	void printfTpl() 
+	{
+		std::cout << "T = " << t_ << std::endl;
+	}
+};
+
 
 int main(int argc,char *argv[])
 {
 	TestTpl<int> tpl(100);
 	
 	std::cout << tpl.getT() << std::endl;
+
+
+	ChildTpl child(3000);
+	child.printfTpl();
 
 	system("pause");
 	return 0;
