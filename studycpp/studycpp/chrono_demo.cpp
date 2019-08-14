b7b390457b757ee898cea11721c5540555f60ecc
rdcentermrzhi@163.com <rdcentermrzhi@163.com>
2019-08-14 09:21:22 +0800

    chrono demo

---
+++ studycpp/studycpp/chrono_demo.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <chrono>
+
+
+class A
+{
+public:
+	A(int a)
+	{
+		std::cout << "A" << std::endl;
+	}
+
+};
+
+
+int main(int argc,char *argv[])
+{
+
+	auto s = std::chrono::system_clock::now();
+
+	auto e = s + std::chrono::minutes(100);
+
+	A(0);
+	
+	system("pause");
+	return 0;
+}
--- studycpp/studycpp/type_trait_demo.cpp
+++ studycpp/studycpp/type_trait_demo.cpp
@@ -8,16 +8,20 @@ struct x_constant {
 	static constexpr T value = v;
 	typedef T value_type;
 	typedef x_constant type; // 使用注入的类名
-	constexpr operator value_type() const noexcept { return value; }
+	constexpr operator value_type() const noexcept { 
+		std::cout << "overload ()" << std::endl;
+		return value;
+	}
 	constexpr value_type operator()() const noexcept { return 5; } // c++14 起
 };
 
-int
-main(int argc, char* argv[])
-{
-
-	std::cout << x_constant<int,4>() << std::endl;
-
-	system("pause");
-	return 0;
-}
+//int
+//main(int argc, char* argv[])
+//{
+//	x_constant<int, 4> x;
+//		int i = x;
+//	std::cout << x << std::endl;
+//
+//	system("pause");
+//	return 0;
+//}
