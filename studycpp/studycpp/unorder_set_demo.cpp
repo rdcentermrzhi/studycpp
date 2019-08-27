9ab63a541b2b51c71bb06700605f1f48ec3998c5
Administrator <Administrator@YCDN-20190418IM>
2019-08-27 21:23:24 +0800

    unorder container bucket demo

--- studycpp/studycpp/unorder_set_demo.cpp
+++ studycpp/studycpp/unorder_set_demo.cpp
@@ -5,6 +5,7 @@
 #include <unordered_set>
 #include <algorithm>
 #include <numeric>
+#include <iomanip>
 using namespace std;
 
 template <class T>
@@ -15,6 +16,45 @@ void print_element(const T& c)
 		cout << elem << " ";
 	}
 	cout << endl;
+}
+
+template <typename T1, typename T2>
+std::ostream& operator << (std::ostream& strm, const std::pair<T1, T2>& p)
+{
+	return strm << "[" << p.first << "." << p.second << "]";
+}
+
+template <typename T>
+void printHashTableState(const T& cont)
+{
+	cout << left;
+	cout << "size:" << cont.size() << "\n";
+	cout << "buckets:" << cont.bucket_count() << "\n";
+	cout << "load factor" << cont.load_factor() << "\n";
+	cout << "max load factor " << cont.max_load_factor() << "\n";
+
+	if (typeid(typename std::iterator_traits<typename T::iterator>::iterator_category) == typeid(std::bidirectional_iterator_tag))
+	{
+		cout << "chaining style: double linked\n";
+	}
+	else
+	{
+		cout << "chaining style: single linked\n";
+	}
+
+	cout << "data:" << "\n";
+	for (auto idx = 0; idx != cont.bucket_count(); ++idx)
+	{
+		cout << "b[" << setw(2) << idx << "]: ";
+		for (auto pos = cont.begin(idx); pos != cont.end(idx); ++pos)
+		{
+			cout << *pos << " ";
+		}
+
+		cout << "\n";
+	}
+
+	cout << "\n";
 }
 
 int
@@ -35,6 +75,8 @@ main(int argc, char**)
 	{
 		cout << "19 avaliable\n";
 	}
+
+	printHashTableState(coll);
 
 	unordered_set<int>::iterator pos;
 	for (pos = coll.begin(); pos != coll.end();)
