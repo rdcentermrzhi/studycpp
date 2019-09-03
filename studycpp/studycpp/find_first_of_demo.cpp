95c407ca65c5c64ae7508974f98b93e24d1ada0b
rdcentermrzhi@163.com <rdcentermrzhi@163.com>
2019-09-03 17:57:05 +0800

    openmp  demo

--- studycpp/studycpp/find_first_of_demo.cpp
+++ studycpp/studycpp/find_first_of_demo.cpp
@@ -10,44 +10,44 @@ bool lessx(int less, int greate)
 	return less < greate;
 }
 
-int
-main(int argc, char**)
-{
-	vector<int> coll;
-	list<int> searchcoll;
-
-	INSERT_ELEMENTS(coll, 1, 11);
-	INSERT_ELEMENTS(searchcoll, 4, 5);
-
-	searchcoll.push_front(2);
-	searchcoll.push_front(2);
-
-
-	PRINT_ELEMENTS(coll, "coll: ");
-	PRINT_ELEMENTS(searchcoll, "searchcoll: ");
-
-	vector<int>::iterator pos;
-	pos = find_first_of(coll.begin(), coll.end(), searchcoll.begin(), searchcoll.end());
-
-	cout << *pos << endl;
-
-	vector<int>::reverse_iterator rpos;
-	rpos = find_first_of(coll.rbegin(), coll.rend(), searchcoll.begin(), searchcoll.end());
-
-	cout << *rpos << endl;
-
-	pos = find_first_of(coll.begin(), coll.end(), searchcoll.begin(), searchcoll.end(), greater<int>());
-
-	cout << *pos << endl;
-
-	list<int>::iterator xpos;
-	xpos = adjacent_find(searchcoll.begin(), searchcoll.end());
-
-	cout << *xpos << endl;
-	system("pause");
-
-
-	
-	
-	return 0;
-}
+//int
+//main(int argc, char**)
+//{
+//	vector<int> coll;
+//	list<int> searchcoll;
+//
+//	INSERT_ELEMENTS(coll, 1, 11);
+//	INSERT_ELEMENTS(searchcoll, 4, 5);
+//
+//	searchcoll.push_front(2);
+//	searchcoll.push_front(2);
+//
+//
+//	PRINT_ELEMENTS(coll, "coll: ");
+//	PRINT_ELEMENTS(searchcoll, "searchcoll: ");
+//
+//	vector<int>::iterator pos;
+//	pos = find_first_of(coll.begin(), coll.end(), searchcoll.begin(), searchcoll.end());
+//
+//	cout << *pos << endl;
+//
+//	vector<int>::reverse_iterator rpos;
+//	rpos = find_first_of(coll.rbegin(), coll.rend(), searchcoll.begin(), searchcoll.end());
+//
+//	cout << *rpos << endl;
+//
+//	pos = find_first_of(coll.begin(), coll.end(), searchcoll.begin(), searchcoll.end(), greater<int>());
+//
+//	cout << *pos << endl;
+//
+//	list<int>::iterator xpos;
+//	xpos = adjacent_find(searchcoll.begin(), searchcoll.end());
+//
+//	cout << *xpos << endl;
+//	system("pause");
+//
+//
+//	
+//	
+//	return 0;
+//}
---
+++ studycpp/studycpp/openmp_demo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+#include"omp.h"
+
+
+using namespace std;
+
+
+void main()
+
+{
+
+#pragma omp parallel for num_threads(6)
+
+	for (int i = 0; i < 12; i++)
+
+	{
+
+		printf("OpenMP Test, Ïß³Ì±àºÅÎª: %d, i= %d\n", omp_get_thread_num(), i);
+
+	}
+
+	system("pause");
+
+}
